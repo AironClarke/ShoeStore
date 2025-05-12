b1c996861d4006ff58f6a0f9af6b9248d888c139
Airon <131386167+AironClarke@users.noreply.github.com>
2025-05-12 12:07:29 +0100

    concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import './normalise.css'
+import './App.css'
+
 function App() {
 
   return (
     <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <div className='carouselFeature'>
+          <div className='carouselFeatureImg parallax'>
+            <img src="/Images/explore-1.jpg" alt="freature image" />
+          </div>
+          <div className='carouselContent'>
+            <div className='carouselContentHeader'>
+              <h3 className='carouselH3'>Our Products</h3>
+              <div className='circle'></div>
+            </div>
+            <div className='carouselReel'>
+              <div className='carouselReelScreen'>
+                <div className='swiper'>
+                  <div className='card'></div>
+                  <div className='card'></div>
+                  <div className='card'></div>
+                  <div className='card'></div>
+                  <div className='card'></div>
+                  <div className='card'></div>
+                  <div className='card'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='carouselFeature'>
+
+
+        </div>
+      </section>
     </>
   )
 }
