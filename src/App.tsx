07a27c3df0bdd48cc95f76ec4870c081adb6e217
Airon <131386167+AironClarke@users.noreply.github.com>
2025-05-13 00:38:15 +0100

    reverse

--- src/App.tsx
+++ src/App.tsx
@@ -99,10 +99,100 @@ function App() {
             </div>
           </div>
         </div>
-        <div className='carouselFeature'>
 
-
-        </div>
+        <div className='carouselFeature reverse'>
+          <div className='carouselFeatureImg parallax'>
+              <img src="/Images/explore-1.jpg" alt="freature image" />
+            </div>
+            <div className='carouselContent'>
+              <div className='carouselContentHeader'>
+                <h3 className='carouselH3'>Our Products</h3>
+                <div className='circle'></div>
+              </div>
+              <div className='carouselReel'>
+                <div className='carouselReelScreen reverse'>
+                  <div className='swiper'>
+                    <div className='card'>
+                      <div className='cardHead'>
+                        <p className='cardHeadTitle'>Pure Brilliance</p>
+                        <div className='cardCircle'></div>
+                      </div>
+                      <div className='cardProductDetails'>
+                        <div className='cardProductImage'>
+                          <img src="Images/item.avif" alt="" />
+                        </div>
+                        <div className='cardFooter'>
+                          <h3>Lorem ipsum dolor <br/>sit amet, 
+                            consectetur adipisicing elit. 
+                          </h3>
+                          <p className='cardPrice'>
+                            £599
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='card'>
+                      <div className='cardHead'>
+                        <p className='cardHeadTitle'>Pure Brilliance</p>
+                        <div className='cardCircle'></div>
+                      </div>
+                      <div className='cardProductDetails'>
+                        <div className='cardProductImage'>
+                          <img src="Images/item.avif" alt="" />
+                        </div>
+                        <div className='cardFooter'>
+                          <h3>Lorem ipsum dolor <br/>sit amet, 
+                            consectetur adipisicing elit. 
+                          </h3>
+                          <p className='cardPrice'>
+                            £599
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='card'>
+                      <div className='cardHead'>
+                        <p className='cardHeadTitle'>Pure Brilliance</p>
+                        <div className='cardCircle'></div>
+                      </div>
+                      <div className='cardProductDetails'>
+                        <div className='cardProductImage'>
+                          <img src="Images/item.avif" alt="" />
+                        </div>
+                        <div className='cardFooter'>
+                          <h3>Lorem ipsum dolor <br/>sit amet, 
+                            consectetur adipisicing elit. 
+                          </h3>
+                          <p className='cardPrice'>
+                            £599
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='card'>
+                      <div className='cardHead'>
+                        <p className='cardHeadTitle'>Pure Brilliance</p>
+                        <div className='cardCircle'></div>
+                      </div>
+                      <div className='cardProductDetails'>
+                        <div className='cardProductImage'>
+                          <img src="Images/item.avif" alt="" />
+                        </div>
+                        <div className='cardFooter'>
+                          <h3>Lorem ipsum dolor <br/>sit amet, 
+                            consectetur adipisicing elit. 
+                          </h3>
+                          <p className='cardPrice'>
+                            £599
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
       </section>
     </>
   )
